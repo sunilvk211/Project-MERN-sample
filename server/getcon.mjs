64068d659e92818b1485314cecad0277cf1ab600
sunilvk211 <118796576+sunilvk211@users.noreply.github.com>
2025-03-03 15:12:37 +0530

    update 2

--- server/getcon.mjs
+++ server/getcon.mjs
@@ -1,5 +1,5 @@
 import { MongoClient } from "mongodb";
-require('dotenv').config();
+import 'dotenv/config';
 const connectionString = process.env.MongoDB_URI;
 
 const client = new MongoClient(connectionString);
