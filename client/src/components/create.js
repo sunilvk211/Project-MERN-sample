7755b99089c298a0f2247b4a4068ec1f36e4db2f
sunilvk211 <118796576+sunilvk211@users.noreply.github.com>
2025-03-03 16:45:28 +0530

    update3

--- client/src/components/create.js
+++ client/src/components/create.js
@@ -23,7 +23,7 @@ export default function Create() {
    // When a post request is sent to the create url, we'll add a new record to the database.
    const newPerson = { ...form };
  
-   await fetch("http://localhost:7000/record", {
+   await fetch("https://project-mern-sample-1.onrender.com/record", {
      method: "POST",
      headers: {
        "Content-Type": "application/json",
--- client/src/components/recordList.js
+++ client/src/components/recordList.js
@@ -21,7 +21,7 @@ export default function RecordList() {
   // This method fetches the records from the database.
   useEffect(() => {
     async function getRecords() {
-      const response = await fetch(`http://localhost:7000/record/`);
+      const response = await fetch(`https://project-mern-sample-1.onrender.com/record/`);
 
       if (!response.ok) {
         const message = `An error occurred: ${response.statusText}`;
@@ -40,7 +40,7 @@ export default function RecordList() {
 
   // This method will delete a record
   async function deleteRecord(id) {
-    await fetch(`http://localhost:7000/record/${id}`, {
+    await fetch(`https://project-mern-sample-1.onrender.com/record/${id}`, {
       method: "DELETE"
     });
 
